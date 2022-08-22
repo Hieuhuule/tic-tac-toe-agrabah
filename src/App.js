601f82ffd209b95e30d5fba85136e11220253829
hieuhuule <hieuhuule@gmail.com>
2022-08-22 13:38:32 -0500

    added non-functional conditional to determine player turn

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState([" "," "," "," "," "," "," "," "," "])
+  const [squares, setSquares] = useState([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  let turnCounter = 0
+  let xTurn = 0
+  let oTurn = 0
+
+  // newSquares[index] = "X"
+  // setSquares(newSquares)
+
+  // Turn counter
+  // if counter is even, X's turn
+  // if counter is odd, O's turn
+  // if counter === 9, gameover
+  // add logic for handleGamePlay to put X or O's
 
   const handleGamePlay = (index) => {
+    
     const newSquares = [...squares]
-    newSquares[index] = "X"
-    setSquares(newSquares)
+    if (xTurn === oTurn) {
+      newSquares[index] = "X"
+      setSquares(newSquares)
+      xTurn++
+      console.log(xTurn)
+    } else if (xTurn > oTurn) {
+      newSquares[index] = "O"
+      setSquares(newSquares)
+      oTurn++
+      console.log(oTurn)
+    } else if (xTurn < oTurn) {
+      newSquares[index] = "X"
+      setSquares(newSquares)
+      xTurn++
+      console.log(xTurn)
+    }
   }
 
 
@@ -16,9 +43,9 @@ const App = () => {
     <>
       <h1>Tic Tac Toe</h1>
       <div className="boardgame" >
-        {squares.map( (value, index) => {
-          return(
-            <Square value ={value} index={index} handleGamePlay={handleGamePlay}/>
+        {squares.map((value, index) => {
+          return (
+            <Square value={value} index={index} handleGamePlay={handleGamePlay} />
           )
         })}
       </div>
